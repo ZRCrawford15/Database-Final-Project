9124a351d03b6f55b1174dd490e6ab29a8882bb2
ZRCrawford15 <48399752+ZRCrawford15@users.noreply.github.com>
2021-07-29 21:09:01 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+var express = require('express');
 let app = express();
 let handlebars = require('express-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'html');
-app.set('port', 4962)
+app.set('port', 4962);
 
 //engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -12,9 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const indexRouter = require('./routes/index')
+const indexRouter = require('./routes/index');
 
-app.use('/', indexRouter)
+app.use('/', indexRouter);
 
 var mysql = require('mysql');
 var pool = mysql.createPool({
@@ -45,7 +46,7 @@ app.get('/galaxies', function(req, res, next) {
     context.results = JSON.stringify(rows);
     res.render('galaxies', context)
   })
-})
+});
 
 // get the hostSystems page
 app.get('/hostSystems', function(req, res, next) {
@@ -58,7 +59,7 @@ app.get('/hostSystems', function(req, res, next) {
     context.results = JSON.stringify(rows);
     res.render('hostSystems', context)
   })
-})
+});
 
 // get the stars page
 app.get('/stars', function(req, res, next) {
@@ -71,7 +72,7 @@ app.get('/stars', function(req, res, next) {
     context.results = JSON.stringify(rows);
     res.render('stars', context)
   })
-})
+});
 
 // get the exoplanets page
 app.get('/exoplanets', function(req, res, next) {
@@ -84,7 +85,7 @@ app.get('/exoplanets', function(req, res, next) {
     context.results = JSON.stringify(rows);
     res.render('exoplanets', context)
   })
-})
+});
 
 // get the M:M exoplanets:stars table page
 app.get('/EPSRelation', function(req, res, next) {
@@ -97,7 +98,7 @@ app.get('/EPSRelation', function(req, res, next) {
     context.results = JSON.stringify(rows);
     res.render('EPSRelation', context)
   })
-})
+});
 
 app.use(function(req,res){
   res.status(404);
